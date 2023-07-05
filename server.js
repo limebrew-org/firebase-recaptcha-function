eb1f970a64f4be78465dc73761d94c3cf0aefdcf
deepraj1729 <deeprajbaidyaofficial@gmail.com>
2023-07-05 22:38:43 +0530

    Added initial server logic

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use("/auth",authRouter)
+app.use("/auth", authRouter)
 
 app.listen(PORT, () => {
 	console.log(
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -3,6 +3,6 @@ import { recaptchaAssessmentProvider } from "../controllers/verifyRecaptcha.js"
 
 const authRouter = express.Router()
 
-authRouter.post("/create-assessment",recaptchaAssessmentProvider)
+authRouter.post("/create-assessment", recaptchaAssessmentProvider)
 
 export { authRouter }
